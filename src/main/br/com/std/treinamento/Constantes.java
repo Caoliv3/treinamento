155707af36478e05cea727a981e5a5cda4baca10
so_carlos@hotmail.com.br <carlos.eduardo@boavistascpc.com.br>
2020-02-14 11:17:18 -0300

    Inclusão da classe Contantes

---
+++ src/main/br/com/std/treinamento/Constantes.java
@@ -0,0 +1,7 @@
+package br.com.std.treinamento;
+
+public interface Constantes {
+
+     String path = "/home/ubuntu/repository/treinamento/src/test/arquivos";
+
+}
--- src/main/br/com/std/treinamento/Exceptions.java
+++ src/main/br/com/std/treinamento/Exceptions.java
@@ -4,19 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Exceptions {
+public class Exceptions implements Constantes{
 
     private int passengers;
-    private String path = "C:\\Users\\carlos\\Documents\\repository\\treinamento\\src\\test\\arquivos";
+   // private String path = "C:\\Users\\carlos\\Documents\\repository\\treinamento\\src\\test\\arquivos";
+   // private String path = "/home/ubuntu/repository/treinamento/src/test/arquivos";
+      private String path1 = null;
 
     public void addPassengers(String filename) throws IOException {
 
       //  arquivo = String.format(path,filename);
-          path = path.concat("\\").concat(filename);
+      //  path = path.concat("\\").concat(filename);
+          path1 = path.concat("/").concat(filename);
 
             BufferedReader reader = null;
             try{
-            reader = new BufferedReader(new FileReader(path));
+            reader = new BufferedReader(new FileReader(path1));
             String line = null;
 
             while((line =reader.readLine()) != null){
